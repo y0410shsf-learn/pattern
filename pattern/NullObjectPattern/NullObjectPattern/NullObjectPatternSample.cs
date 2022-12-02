fcc9bdbee7a85d506119a8cade6a7f1104a9956d
yu0410 <yu0410aries@gmail.com>
2022-12-02 08:53:31 +0800

    init:

---
+++ pattern/NullObjectPattern/NullObjectPattern/NullObjectPatternSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectPattern
+{
+    interface NullObjectPatternSample
+    {
+        void foo();
+    }
+
+    class NullObject : NullObjectPatternSample
+    {
+        public void foo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class RealObject : NullObjectPatternSample
+    {
+        public void foo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ pattern/NullObjectPattern/NullObjectPattern/readme.md
@@ -0,0 +1,5 @@
+﻿至少會有一個 real object，一個 null object，以及他們共通的繼承對象 abstract object，呼叫者
+會透過 abstract object 的介面來使用 read object 或是 null object。這種使用方式就是熟悉的多型(polymorphism)
+
+先看 NullObjectPatternSample.cs
+
